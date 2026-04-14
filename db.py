915fe9a224a56cf4f6d7810a6dd54f858f38fed1
Niko Pitkänen <niko.pitkanen.git@outlook.com>
2026-04-14 12:18:28 +0300

    feat/added function to create database tables

--- db.py
+++ db.py
@@ -12,22 +12,30 @@ create_tables = [
     )
     """,
     """
+    CREATE TABLE IF NOT EXISTS milestones (
+    milestone_id INTEGER PRIMARY KEY,
+    project_id INTEGER,
+    created_at DATE,
+    milestone_name TEXT,
+    date_start DATE,
+    date_end DATE,
+    FOREIGN KEY (project_id) REFERENCES projects (project_id)
+    )
+    """,
+    """
     CREATE TABLE IF NOT EXISTS tasks (
     task_id INTEGER PRIMARY KEY,
+    project_id INTEGER,
+    milestone_id INTEGER,
+    parent_task_id INTEGER,
     created_at DATE,
     task_name TEXT,
     task_description TEXT,
     date_start DATE,
-    date_end DATE
-    )
-    """,
-    """
-    CREATE TABLE IF NOT EXISTS milestones (
-    milestone_id INTEGER PRIMARY KEY,
-    created_at DATE,
-    milestone_name TEXT,
-    date_start DATE,
-    date_end DATE
+    date_end DATE,
+    FOREIGN KEY (project_id) REFERENCES projects (project_id) ON DELETE CASCADE,
+    FOREIGN KEY (milestone_id) REFERENCES milestones (milestone_id),
+    FOREIGN KEY (parent_task_id) REFERENCES tasks (task_id) ON DELETE CASCADE
     )
     """,
 ]
@@ -42,3 +50,6 @@ def init_db() -> None:
         print("Database initialized successfully.")
     except sqlite3.OperationalError as e:
         print(f"Failed to initialize Database: {e}")
+
+
+init_db()
